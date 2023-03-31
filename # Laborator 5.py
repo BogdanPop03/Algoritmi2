fe84e82e6016f6a12c28b655828a00bbdb169469
BogdanPop03 <pop.bogdan01022003@gmail.com>
2023-03-31 08:36:05 +0300

    ex1 var 1 si var 2

--- # Laborator 5.py
+++ # Laborator 5.py
@@ -21,16 +21,55 @@ def ex1_var1(lista: list) -> int:
     return prod_max, prod_min
 
 
-if __name__ == '__main__':
-    print("Exemplul 1:")
+def ex1_var2(lista: list) -> int:
+    lista.sort()
     
-    n = int(input("Introduceti numarul de elemente al listei: "))
-    lista = []
-    
-    for indice in range(n):
-        lista.append(int(input(f'Introduceti elementul {indice + 1} al listei: ')))
+    if len(lista) >= 4:
+        min1, min2 = lista[0], lista[1]
+        max1, max2 = lista[len(lista) - 1], lista[len(lista) - 2]
         
-    prod_max = ex1_var1(lista)[0]
-    prod_min = ex1_var1(lista)[1]
+        prod_max = max1 * max2
+        prod_min = min1 * min2
+        
+        return prod_max, prod_min
+    else:
+        print("Lista nu are suficiente elemente.")
+        return
+
+
+if __name__ == '__main__':
+    numar_ex: int = int(input("Introduceti numarul exemplului dorit: "))
     
-    print(f'Produsul maxim este: {prod_max}\nProdusul minim este: {prod_min}')
+    if numar_ex == 1:
+        print("Exemplul 1:")
+        
+        print("Varianta 1:")
+        
+        n = int(input("Introduceti numarul de elemente al listei: "))
+        lista = []
+        
+        for indice in range(n):
+            lista.append(int(input(f'Introduceti elementul {indice + 1} al listei: ')))
+            
+        prod_max = ex1_var1(lista)[0]
+        prod_min = ex1_var1(lista)[1]
+        
+        print(f'Produsul maxim este: {prod_max}\nProdusul minim este: {prod_min}')
+        
+        print("Varianta 2:")
+        
+        n = int(input("Introduceti numarul de elemente al listei: "))
+        lista = []
+        
+        for indice in range(n):
+            lista.append(int(input(f'Introduceti elementul {indice + 1} al listei: ')))
+        
+        if len(lista) >= 4:    
+            prod_max = ex1_var2(lista)[0]
+            prod_min = ex1_var2(lista)[1]
+            
+            print(f'Produsul maxim este: {prod_max}\nProdusul minim este: {prod_min}')
+        else:
+            print(ex1_var2(lista))
+            
+    elif numar_ex == 2:
