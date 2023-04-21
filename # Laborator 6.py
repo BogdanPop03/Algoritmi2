f3db9e82a49e9ce2720fa44e3a48ded65591f576
BogdanPop03 <pop.bogdan01022003@gmail.com>
2023-04-21 09:53:09 +0300

    Dictionary

--- # Laborator 6.py
+++ # Laborator 6.py
@@ -12,7 +12,48 @@ def selectionSort(lista: list) -> list:
             if lista[j] < lista[minIndexValue]:
                 minIndexValue = j
                 
-        if minIndexValue != j:
+        if minIndexValue != i:
             lista[i], lista[minIndexValue] = lista[minIndexValue], lista[i]
         
     return lista
+
+# Dictionaries
+
+employee1 = {
+    'id': 14,
+    'name': 'John Doe',
+    'age': 36,
+    'position': 'Business Manager'
+}
+
+employee2 = {
+    'id': 2,
+    'name': 'Jane Doe',
+    'age': 20,
+    'position': 'N/A'
+}
+
+employee3 = {
+    'id': 110,
+    'name': 'John Smith',
+    'age': 40,
+    'position': 'Software Developer'
+}
+
+employee4 = {
+    'id': 40,
+    'name': 'Jane Smith',
+    'age': 35,
+    'position': 'Engineer'
+}
+
+lista = [employee1, employee2, employee3, employee4]
+idCautat = 5
+
+def dictionarySearch():
+    for indice in range(len(lista)):
+        if lista[indice]['id'] == idCautat:
+            return lista[indice]['position']
+    else:
+        return None
+        
