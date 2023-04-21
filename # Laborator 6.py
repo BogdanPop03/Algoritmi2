e964bb2eb4dd3177b86bf77257cf3c4519427f09
BogdanPop03 <pop.bogdan01022003@gmail.com>
2023-04-21 08:36:27 +0300

    Selection sort

--- # Laborator 6.py
+++ # Laborator 6.py
@@ -1,2 +1,18 @@
 # Laborator 6
 
+# Selection sort
+
+def selectionSort(lista: list) -> list:
+    dim = len(lista)
+    
+    for i in range(dim):
+        minIndexValue = i
+        
+        for j in range(i + 1, dim):
+            if lista[j] < lista[minIndexValue]:
+                minIndexValue = j
+                
+        if minIndexValue != j:
+            lista[i], lista[minIndexValue] = lista[minIndexValue], lista[i]
+        
+    return lista
