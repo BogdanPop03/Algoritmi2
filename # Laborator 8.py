db77c701659bd169bb8fd1d90a748622d76f1066
BogdanPop03 <pop.bogdan01022003@gmail.com>
2023-05-05 08:50:23 +0300

    exemplu 1

--- # Laborator 8.py
+++ # Laborator 8.py
@@ -1,2 +1,21 @@
 # Laborator 8
 
+# Exemplu 1
+def ex1():
+    file = open("lab8_1.txt", "a+")
+    
+    while True:
+        input_line = input("Introduceti o linie de text sau \"stop\" pentru a opri citirea: ")
+        if input_line == "stop":
+            break
+        file.write(input_line + '\n')
+        
+    file.close()
+    
+    file = open("lab8_1.txt", "r")
+        
+    print(file.read())
+    
+    
+if __name__ == "__main__":
+    ex1()
