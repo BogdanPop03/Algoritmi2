c3ad0a1a7ead7eeda4096331042a45fc6793903e
BogdanPop03 <pop.bogdan01022003@gmail.com>
2023-05-05 09:34:35 +0300

    exemplu 2

--- # Laborator 8.py
+++ # Laborator 8.py
@@ -14,8 +14,39 @@ def ex1():
     
     file = open("lab8_1.txt", "r")
         
-    print(file.read())
+    print(file.read(), end = "")
+    
+
+def ex2():
+    file = open("lab8_2.txt", "r")
+    
+    linie1 = file.readline().strip().split(" ")
+    
+    nr_linii = int(linie1[0])
+    nr_coloane = int(linie1[1])
+    
+    matrice = []
+    
+    for linie in range(nr_linii):
+        aux = file.readline().strip().split(" ")
+        aux = list(map(int, aux))
+        matrice.append(aux)
+    
+    file.close()
+        
+    file = open("lab8_1.txt", "a")
+    
+    file.writelines(str(matrice))
+    
+    file.close()
+    
+    file = open("lab8_1.txt", "r")
+    
+    print(file.read(), end = "")
     
     
 if __name__ == "__main__":
+    print("ex1: ")
     ex1()
+    print("\nex2: ")
+    ex2()
