21d9695595edf11c29566f73e8d789c66f5fa247
BogdanPop03 <pop.bogdan01022003@gmail.com>
2023-05-08 18:20:48 +0300

    meh

---
+++ # Curs_Data_8_05_2023.py
@@ -0,0 +1,40 @@
+# Curs_Data_8_05_2023
+
+import random
+import math
+
+# Aproximare pi
+def aproximarePi():
+    def isInsideTheCircle(coord_x: float, coord_y: float) -> bool:
+        if math.sqrt((coord_x - 1) ** 2 + (coord_y - 1) ** 2) <= 1:
+            return True
+        return False
+
+    inside_the_circle: int = int(0)
+    inside_the_square: int = int(0)
+
+    for count in range(2_500_000):
+        coord_x: float = random.uniform(0, 2)
+        coord_y: float = random.uniform(0, 2)
+        
+        if isInsideTheCircle(coord_x, coord_y):
+            inside_the_circle += 1
+        
+        inside_the_square += 1
+        
+
+    pi: float = 4 * inside_the_circle / inside_the_square
+
+    print(f'The aproximate value of pi is: {pi}')
+
+
+def comisVoiajor():
+    
+    
+    return
+
+
+if __name__ == "__main__":
+    aproximarePi()
+    
+    
