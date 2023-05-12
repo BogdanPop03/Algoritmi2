615b6e78eac5236dbc51fa45f5fc8d760b3984f7
BogdanPop03 <pop.bogdan01022003@gmail.com>
2023-05-12 09:43:04 +0300

    Laborator 9 end

--- # Laborator 9.py
+++ # Laborator 9.py
@@ -19,8 +19,6 @@ def matriceAdiacenta(lista_legaturi: list) -> list:
     
     matrice = [[0 for coloana in range(nr_noduri)] for rand in range(nr_noduri)]
     
-    # print(matrice)
-    
     for lista_aux in lista_legaturi:
         matrice[lista_aux[0]][lista_aux[1]], matrice[lista_aux[1]][lista_aux[0]] = 1, 1
         
@@ -31,6 +29,12 @@ def listaAdiacenta(matrice_adiacenta: list):
     lista_adiacenta = [(rand, coloana) for rand in range(len(matrice_adiacenta)) for coloana in range(len(matrice_adiacenta)) if matrice_adiacenta[rand][coloana] != 0 and coloana > rand]
     
     return lista_adiacenta
+
+# test
+def ul(numar: float):
+    numar = int(numar)
+    
+    return numar % 10
 
     
 if __name__ == "__main__":
@@ -45,4 +49,7 @@ if __name__ == "__main__":
     print('Lista de adiacenta: ')
     for linie in lista_adiacenta:
         print(f'{linie}', end=' ')
+        
+    print(ul(1001.56))
+    print(ul(123.45))
                 
