ccdf8760d080d18e581c93014bf17608566eafe9
BogdanPop03 <pop.bogdan01022003@gmail.com>
2023-05-12 07:54:02 +0300

    Laborator 9 start

--- # Curs_Data_8_05_2023.py
+++ # Curs_Data_8_05_2023.py
@@ -27,14 +27,63 @@ def aproximarePi():
 
     print(f'The aproximate value of pi is: {pi}')
 
+import heapq
 
 def comisVoiajor():
+    matrix = []
     
+    with open("comis_voiajor_in.txt" , "r") as file_in:
+        for line in file_in:
+            values = line.split()
+            matrix.append([float(value) for value in values])
+            
+    file_in.close()
     
-    return
+    # print(matrix)
+    
+    start = 0
+    end = 11
+    
+    distance = {i: float('inf') for i in range(len(matrix))}
+    distance[start] = 0
 
+    # Initialize the path dictionary with empty paths for all nodes
+    path = {i: [] for i in range(len(matrix))}
+    path[start] = [start]
+
+    # Initialize the heap with the starting node and its distance
+    heap = [(0, start)]
+
+    # Initialize the visited set
+    visited = set()
+
+    # While there are still nodes to visit
+    while heap:
+        # Pop the node with the smallest distance from the heap
+        (dist, node) = heapq.heappop(heap)
+
+        # If we've already visited this node, skip it
+        if node in visited:
+            continue
+
+        # Otherwise, mark it as visited
+        visited.add(node)
+
+        # Update the distances and paths of all neighboring nodes
+        for neighbor, weight in enumerate(matrix[node]):
+            if weight == 0:
+                continue
+            new_distance = dist + weight
+            if new_distance < distance[neighbor]:
+                distance[neighbor] = new_distance
+                path[neighbor] = path[node] + [neighbor]
+                heapq.heappush(heap, (new_distance, neighbor))
+
+    # Return the shortest path from the starting node to the end node
+    return path[end]
+    
 
 if __name__ == "__main__":
-    aproximarePi()
+    # aproximarePi()
     
-    
+    print(comisVoiajor())
