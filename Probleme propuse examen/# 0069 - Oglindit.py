497b3ba633b3b6a698cbed1790d7d1dbdbaebb17
BogdanPop03 <pop.bogdan01022003@gmail.com>
2023-06-05 16:54:14 +0300

    validare date prooblema 3

--- Probleme propuse examen/# 0069 - Oglindit.py
+++ Probleme propuse examen/# 0069 - Oglindit.py
@@ -1,11 +1,29 @@
 # 0069 - Oglindit
 
+def validareDate(numar):
+    try:
+        number = int(numar)
+
+        if number < 0 or number > 1_000_000_000:
+            raise ValueError("Number is outside the valid range.")
+
+        print("Valid number entered:", number)
+        
+        return str(numar)
+
+    except ValueError as e:
+        print("Invalid input:", str(e))
+        return
+
 def solutie(numar):
     numar = numar[::-1]
     
-    return numar
+    print(numar)
 
 if __name__ == "__main__":
-    numar = int(input())
+    numar = input()
     
-    print(solutie(str(numar)))
+    numar = validareDate(numar)
+    
+    if numar is not None:
+        solutie(numar)
