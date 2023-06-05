ebee8b0bd2fe81c229a560ef5795f3031846dfe2
BogdanPop03 <pop.bogdan01022003@gmail.com>
2023-06-05 17:26:36 +0300

    validare date problema 7

--- Probleme propuse examen/# 2689 - Pal XXL.py
+++ Probleme propuse examen/# 2689 - Pal XXL.py
@@ -1,10 +1,48 @@
 # 2689 - Pal XXL
 
+def validareDate(numar, key):
+    match key:
+        case 'case1':
+            try:
+                number = int(numar)
+
+                if number < 1 or number > 1_000:
+                    raise ValueError("Number is outside the valid range.")
+
+                print("Valid numbers entered:", number)
+                
+                return int(numar)
+
+            except ValueError as e:
+                print("Invalid input:", str(e))
+                return
+        case 'case2':
+            try:
+                number = int(numar)
+
+                if number < 0 or number > 9:
+                    raise ValueError("Number is outside the valid range.")
+
+                print("Valid numbers entered:", number)
+                
+                return int(numar)
+
+            except ValueError as e:
+                print("Invalid input:", str(e))
+                return
+
 def solutie(numar):
     lista = []
     
     for indice in range(numar):
-        lista.append(int(input()))
+        element = input()
+        
+        element = validareDate(element, 'case2')
+        
+        if element is not None:
+            lista.append(element)
+        else:
+            return
         
     numar_str = ""
     
@@ -12,11 +50,14 @@ def solutie(numar):
         numar_str += str(lista[indice])
         
     if numar_str == numar_str[::-1]:
-        return "DA"
+        print("DA") 
     else:
-        return "NU"
+        print("NU")
 
 if __name__ == "__main__":
-    numar = int(input())
+    numar = input()
     
-    print(solutie(numar))
+    numar = validareDate(numar, 'case1')
+    
+    if numar is not None:
+        solutie(numar)
