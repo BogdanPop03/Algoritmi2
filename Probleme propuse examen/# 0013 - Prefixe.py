b97aeef10c0ce84a2200afbdd3af3ad83b051df7
BogdanPop03 <pop.bogdan01022003@gmail.com>
2023-06-05 18:12:05 +0300

    validare date problema 12

--- Probleme propuse examen/# 0013 - Prefixe.py
+++ Probleme propuse examen/# 0013 - Prefixe.py
@@ -1,5 +1,21 @@
 # 0013 - Prefixe
 
+def validareDate(text):
+    try:
+        if len(text) > 10:
+            raise ValueError("Text length should be at most 10.")
+
+        if not text.islower():
+            raise ValueError("Text should consist only of lowercase letters.")
+
+        print("Valid input:", text)
+        
+        return text
+
+    except ValueError as e:
+        print("Invalid input:", str(e))
+        return
+
 def solutie(text):
     for indice in reversed(range(len(text))):
         print(text[:indice])
@@ -9,4 +25,7 @@ def solutie(text):
 if __name__ == "__main__":
     text = input()
     
-    solutie(text)
+    text = validareDate(text)
+    
+    if text is not None:
+        solutie(text)
