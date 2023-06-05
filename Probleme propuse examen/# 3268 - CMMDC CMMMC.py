c2b69d30d6c0ccbd0722c8bd6bcc1b76857b5da0
BogdanPop03 <pop.bogdan01022003@gmail.com>
2023-06-05 16:59:21 +0300

    validare date problema 4

--- Probleme propuse examen/# 3268 - CMMDC CMMMC.py
+++ Probleme propuse examen/# 3268 - CMMDC CMMMC.py
@@ -1,5 +1,20 @@
 # 3268 - CMMDC CMMMC
 
+def validareDate(numar):
+    try:
+        number = int(numar)
+
+        if number < 0 or number > 1_000_000_000:
+            raise ValueError("Number is outside the valid range.")
+
+        print("Valid number entered:", number)
+        
+        return int(numar)
+
+    except ValueError as e:
+        print("Invalid input:", str(e))
+        return
+
 def solutie(numar1, numar2):
     cmmdc, cmmmc = 0, 0
     copie1, copie2 = numar1, numar2
@@ -15,10 +30,13 @@ def solutie(numar1, numar2):
     cmmmc = (copie1 * copie2) // cmmdc
     
     
-    return cmmdc, cmmmc
+    print(cmmdc, cmmmc)
 
 if __name__ == "__main__":
-    numar1 = int(input())
-    numar2 = int(input())
+    numar1 = input()
+    numar2 = input()
     
-    print(solutie(numar1, numar2))
+    numar1, numar2 = validareDate(numar1), validareDate(numar2)
+    
+    if numar1 is not None and numar2 is not None:
+        solutie(numar1, numar2)
