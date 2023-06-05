cc80075bbb9d2c275da5716c3023652df878b87f
BogdanPop03 <pop.bogdan01022003@gmail.com>
2023-06-05 16:31:57 +0300

    validare date problema 1

--- Probleme propuse examen/# 0336 - Expresie 4.py
+++ Probleme propuse examen/# 0336 - Expresie 4.py
@@ -1,16 +1,32 @@
 # 0336 - Expresie 4
 
+def validareDate(numar):
+    try:
+        number = int(numar)
+
+        if number < 1 or number > 100:
+            raise ValueError("Number is outside the valid range.")
+
+        print("Valid number entered:", number)
+        
+        return numar
+
+    except ValueError as e:
+        print("Invalid input:", str(e))
+        return
+
 def solutie(numar):
     raspuns = 0
     
     for indice in range(numar):
         raspuns += (indice + 1) * (numar - indice)
 
-    return raspuns        
+    print(raspuns)         
 
 if __name__ == "__main__":
     numar = int(input())
     
-    raspuns = solutie(numar)
+    numar = validareDate(numar)
     
-    print("Raspunsul este:", raspuns)
+    if numar is not None:
+        solutie(numar)
