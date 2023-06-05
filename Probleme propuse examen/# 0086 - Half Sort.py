00979c235bcfb5e7f64227420cd0bf7215f49fc5
BogdanPop03 <pop.bogdan01022003@gmail.com>
2023-06-05 17:47:58 +0300

    validare date problema 8

--- Probleme propuse examen/# 0086 - Half Sort.py
+++ Probleme propuse examen/# 0086 - Half Sort.py
@@ -1,17 +1,58 @@
 # 0086 - Half Sort
 
+def validareDate(numar, key):
+    match key:
+        case 'case1':
+            try:
+                number = int(numar)
+
+                if number < 1 or number > 100 or number % 2:
+                    raise ValueError("Number is outside the valid range.")
+
+                print("Valid numbers entered:", number)
+                
+                return int(numar)
+
+            except ValueError as e:
+                print("Invalid input:", str(e))
+                return
+        case 'case2':
+            try:
+                number = int(numar)
+
+                if number < -1_000_000_000 or number > 1_000_000_000:
+                    raise ValueError("Number is outside the valid range.")
+
+                print("Valid numbers entered:", number)
+                
+                return int(numar)
+
+            except ValueError as e:
+                print("Invalid input:", str(e))
+                return
+
 def solutie(numar):
     lista = []
     
     for indice in range(numar):
-        lista.append(int(input()))
+        element = input()
+        
+        element = validareDate(element, 'case2')
+        
+        if element is not None:
+            lista.append(element)
+        else:
+            return
         
     lista[:len(lista) // 2] = sorted(lista[:len(lista) // 2])
     lista[len(lista) // 2:] = sorted(lista[len(lista) // 2:], reverse=True)
     
-    return lista
+    print(*lista, sep = ' ') 
 
 if __name__ == "__main__":
-    numar = int(input())
+    numar = input()
     
-    print(solutie(numar))
+    numar = validareDate(numar, 'case1')
+    
+    if numar is not None:
+        solutie(numar)
