60b15db12185f4ad357d182389ade840a1f41b43
BogdanPop03 <pop.bogdan01022003@gmail.com>
2023-06-05 18:07:31 +0300

    validare date problema

--- Probleme propuse examen/# 2841 - Gen Mat 27.py
+++ Probleme propuse examen/# 2841 - Gen Mat 27.py
@@ -1,9 +1,25 @@
 # 2841 - Gen Mat 27
 
+def validareDate(numar):
+    try:
+        number = int(numar)
+
+        if number < 2 or number > 20:
+            raise ValueError("Number is outside the valid range.")
+
+        print("Valid numbers entered:", number)
+
+        return int(numar)
+
+    except ValueError as e:
+        print("Invalid input:", str(e))
+        return
+
+
 def solutie(numar1, numar2):
     matrice = []
     aux = 0
-    
+
     for indice1 in range(numar1):
         linie = []
         for indice2 in range(numar2):
@@ -12,13 +28,15 @@ def solutie(numar1, numar2):
                 aux += 1
             linie.append(aux * 2)
         matrice.append(linie)
-    
-    return matrice
 
-if __name__ == "__main__":
-    numar1, numar2 = int(input()), int(input())
-    
-    matrice = solutie(numar1, numar2)
-    
     for indice in range(numar1):
         print(*matrice[indice], sep=', ')
+
+
+if __name__ == "__main__":
+    numar1, numar2 = input(), input()
+    
+    numar1, numar2 = validareDate(numar1), validareDate(numar2)
+
+    if numar1 is not None and numar2 is not None:
+        solutie(numar1, numar2)
