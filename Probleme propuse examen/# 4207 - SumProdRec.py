085cb192ca301bf158d2d68be8d184d95dd5bc2f
BogdanPop03 <pop.bogdan01022003@gmail.com>
2023-06-05 18:14:31 +0300

    validare date problema 11

--- Probleme propuse examen/# 4207 - SumProdRec.py
+++ Probleme propuse examen/# 4207 - SumProdRec.py
@@ -1,5 +1,20 @@
 # 4207 - SumProdRec
 
+def validareDate(numar):
+    try:
+        number = int(numar)
+
+        if number < 2 or number > 10_000:
+            raise ValueError("Number is outside the valid range.")
+
+        print("Valid numbers entered:", number)
+
+        return int(numar)
+
+    except ValueError as e:
+        print("Invalid input:", str(e))
+        return
+
 def solutie(numar):
     if numar == 1:
         return 0
@@ -7,6 +22,9 @@ def solutie(numar):
         return (numar - 1) * numar + solutie(numar - 1)
 
 if __name__ == "__main__":
-    numar = int(input())
+    numar = input()
     
-    print(solutie(numar))
+    numar = validareDate(numar)
+    
+    if numar is not None:
+        print(solutie(numar))
