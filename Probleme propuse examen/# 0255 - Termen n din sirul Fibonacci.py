42111f70d60f0e5e2120789cc00d83f725a10694
BogdanPop03 <pop.bogdan01022003@gmail.com>
2023-06-05 17:01:13 +0300

    validare date problema 5

--- Probleme propuse examen/# 0255 - Termen n din sirul Fibonacci.py
+++ Probleme propuse examen/# 0255 - Termen n din sirul Fibonacci.py
@@ -1,21 +1,39 @@
 # 0255 - Termen n din sirul Fibonacci
 
+def validareDate(numar):
+    try:
+        number = int(numar)
+
+        if number < 1 or number > 500_000_000:
+            raise ValueError("Number is outside the valid range.")
+
+        print("Valid number entered:", number)
+        
+        return int(numar)
+
+    except ValueError as e:
+        print("Invalid input:", str(e))
+        return
+
 def solutie(numar):
     fibo1, fibo2 = 0, 1
     fibo3 = 0
     
     if numar == 1:
-        return fibo1
+        print(fibo1)
     elif numar == 2:
-        return fibo2
+        print(fibo2)
     else:
         for i in range(2, numar ):
             fibo3 = fibo1 + fibo2
             fibo1, fibo2 = fibo2, fibo3
     
-        return fibo3
+        print(fibo3)
 
 if __name__ == "__main__":
-    numar = int(input())
+    numar = input()
     
-    print(solutie(numar))
+    numar = validareDate(numar)
+    
+    if numar is not None:
+        solutie(numar)
